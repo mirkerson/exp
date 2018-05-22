c1d1a9807a1d10f85063686539a6b5020ad2e26a
kerson <252915145@qq.com>
2018-05-22 14:26:53 +0800

    add qt

--- build.sh
+++ build.sh
@@ -6,19 +6,9 @@ UBOOT_DIR=$TOP_DIR/u-boot
 KERNEL_DIR=$TOP_DIR/linux
 SUNXI_TOOLS_DIR=$TOP_DIR/sunxi-tools
 BUILDROOT_DIR=$TOP_DIR/buildroot-2017.08
-MINIGUI_DIR=$TOP_DIR/minigui
-ZLIB_DIR=$MINIGUI_DIR/zlib-1.2.11
-LIBPNG_DIR=$MINIGUI_DIR/libpng-1.2.37
-TSLIB_DIR=$MINIGUI_DIR/tslib-1.15
-LIBMINIGUI_DIR=$MINIGUI_DIR/libminigui-3.0.12-linux
-MGPLUS_DIR=$MINIGUI_DIR/libmgplus-1.2.4
-MG_SAMPLES_DIR=$MINIGUI_DIR/mg-samples-3.0.12
-MINIGUI_RES_DIR=$MINIGUI_DIR/minigui-res-be-3.0.12
 TARGET_DIR=$TOP_DIR/target
 
-MINIGUI_TARGET_DIR=$TOP_DIR/minigui/target
-MINIGUI_LIB_DIR=$MINIGUI_TARGET_DIR/lib
-MINIGUI_INC_DIR=$MINIGUI_TARGET_DIR/include
+QT_SRC_DIR=$TOP_DIR/qt-everywhere-opensource-src-4.8.6
 
 function build_sunxi_tools()
 {
@@ -96,8 +86,12 @@ function clean_kernel()
 
 function build_buildroot()
 {
+	echo "Build buildroot"
+
 	cd $BUILDROOT_DIR
 	
+	make distclean
+
 	cp configs/mangopi_buildroot_config .config
 
 	make 
@@ -107,7 +101,7 @@ function build_buildroot()
 		exit
 	fi
 	
-	rm -r $TARGET_DIR
+	rm -rf $TARGET_DIR
 
 	mkdir -p $TARGET_DIR
 	
@@ -122,268 +116,113 @@ function clean_buildroot()
 	make clean
 }
 
-function build_zlib()
+function build_qt()
 {
-	###build zlib
-	cd $ZLIB_DIR
-	export CC=arm-linux-gnueabihf-gcc
+	echo "==========Start Build QT=========="
 	
-	./configure --shared --prefix=$MINIGUI_TARGET_DIR --libdir=$MINIGUI_LIB_DIR --includedir=$MINIGUI_INC_DIR
+	cd $QT_SRC_DIR
+
+	rm -rf instal
+	mkdir instal
+
+	./configure \
+	-prefix $QT_SRC_DIR/instal \
+	-confirm-license \
+	-embedded arm \
+	-xplatform qws/arm-linux-gnueabihf-g++ \
+	-release \
+	-opensource  \
+	-fast  \
+	-no-accessibility  \
+	-no-scripttools  \
+	-no-mmx  \
+	-no-multimedia  \
+	-no-svg  \
+	-no-3dnow  \
+	-no-sse  \
+	-no-sse2  \
+	-no-libmng  \
+	-no-libtiff  \
+	-no-multimedia  \
+	-silent  \
+	-qt-libpng  \
+	-qt-libjpeg  \
+	-make libs  \
+	-nomake translations \
+	-no-nis \
+	-no-cups \
+	-no-iconv  \
+	-no-dbus  \
+	-no-openssl  \
+	-little-endian \
+	-qt-freetype  \
+	-depths all \
+	-qt-gfx-linuxfb  \
+	-no-gfx-transformed  \
+	-no-gfx-multiscreen  \
+	-no-gfx-vnc  \
+	-no-gfx-qvfb  \
+	-qt-kbd-linuxinput  \
+	-no-glib  \
+	-qt-zlib \
+	-no-phonon \
+	-no-phonon-backend \
+	-no-javascript-jit \
+	-no-sql-db2  \
+	-no-sql-ibase \
+	-no-sql-oci \
+	-no-sql-odbc \
+	-no-sql-psql \
+	-qt-sql-sqlite \
+	-plugin-sql-sqlite \
+	-no-sql-sqlite2 \
+	-no-sql-mysql \
+	-no-sql-tds \
+	-no-qt3support \
+	-qt-mouse-linuxinput \
+	-no-mouse-linuxtp \
+	-no-script \
+	-no-largefile \
+	-nomake docs  \
+	-I$MINIGUI_INC_DIR \
+	-L$MINIGUI_LIB_DIR \
+	-D QT_QWS_CLIENTBLIT \
+	-qt-mouse-linuxinput
+	-no-mouse-linuxtp \
+	-plugin-mouse-tslib \
+	-nomake tools \
+	-nomake docs  \
+	-no-webkit \
+	-stl \
+
+#	-nomake demos \
+#	-nomake examples \
+
+	make clean
+	make -j8
+	make install
 	
-	if [ $? != "0" ] ; then
-		echo "zlib configure error!"
-		exit
-	fi
-
-	make 
-
-	if [ $? != "0" ] ; then
-		echo "build zlib error!"
-		exit
-	fi
-
-	make install
-
 }
 
-function clean_zlib()
+function clean_qt()
 {
-	cd $ZLIB_DIR
+	cd $QT_DIR
+	rm -rf instal
 	make distclean
 }
 
-function build_libpng()
+function build_qt_demos()
 {
-	###build libpng
-	cd $LIBPNG_DIR
-	./configure --prefix=$MINIGUI_TARGET_DIR \
-		CC=arm-linux-gnueabihf-gcc \
-		 --host=arm-linux --build=i386-linux --enable-shared\
-		 LDFLAGS=-L$MINIGUI_LIB_DIR \
-		 CFLAGS=-I$MINIGUI_INC_DIR \
-		 LIBS=-lz
-
-	if [ $? != "0" ] ; then
-		echo "libpng configure error!"
-		exit
-	fi
-
-	make 
-
-	if [ $? != "0" ] ; then
-		echo "build libpng error!"
-		exit
-	fi
-
-	make install
-
-}
-
-function clean_libpng()
-{
-	cd $LIBPNG_DIR
-	make distclean
-}
-
-function build_tslib()
-{
-	###build tslib
-	cd $TSLIB_DIR
+	echo "==========Start Build QT Demos=========="
 	
-	autoreconf -ivf
-
-	./configure --host=arm-linux ac_cv_func_malloc_0_nonnull=yes \
-		 --enable-linear=static --enable-input=static \
-		 --enable-pthres=static --enable-variance=static --enable-dejitter=static \
-		 -prefix=$MINIGUI_TARGET_DIR CC=arm-linux-gnueabihf-gcc 
-
-	if [ $? != "0" ] ; then
-		echo "tslib configure error!"
-		exit
-	fi
-
-	make 
-
-	if [ $? != "0" ] ; then
-		echo "build tslib error!"
-		exit
-	fi
-
-	make install
-
-}
-
-function clean_tslib()
-{
-	cd $TSLIB_DIR
-	make distclean
-}
-
-function build_libminigui()
-{
-	###build libminigui
-	cd $LIBMINIGUI_DIR
-	./configure --prefix=$MINIGUI_TARGET_DIR \
-		CC=arm-linux-gnueabihf-gcc \
-		 --host=arm-linux --build=i386-linux --target=arm-linux --with-osname=linux \
-                 --with-targetname=fbcon \
-		 --enable-videofbcon \
-		 --enable-autoial  \
-		 --enable-tslibial  \
-		 --disable-vbfsupport  \
-		 --disable-screensaver \
-		 --disable-pcxvfb \
-	   	 --disable-dlcustomial \
-		 --disable-cursor \
-		 --enable-pngsupport \
-		 CFLAGS=-I$MINIGUI_INC_DIR \
-		 LDFLAGS=-L$MINIGUI_LIB_DIR 
-
-	if [ $? != "0" ] ; then
-		echo "libminigui configure error!"
-		exit
-	fi
-
-	make 
-
-	if [ $? != "0" ] ; then
-		echo "build libminigui error!"
-		exit
-	fi
-
-	make install
-
-}
-
-function clean_libminigui()
-{
-	cd $LIBMINIGUI_DIR
-	make distclean
-}
-
-function build_mgplus()
-{
-	###build mgplus
-	cd $MGPLUS_DIR
-	./configure --prefix=$MINIGUI_TARGET_DIR \
-		CC=arm-linux-gnueabihf-gcc \
-		CXX=arm-linux-gnueabihf-g++ \
-		 --host=arm-linux --build=i386-linux --target=arm-linux \
-		 CFLAGS=-I$MINIGUI_INC_DIR \
-		 CXXFLAGS=-I$MINIGUI_INC_DIR \
-		 LDFLAGS=-L$MINIGUI_LIB_DIR \
-		 MINIGUI_CFLAGS=-I$MINIGUI_INC_DIR \
-		 MINIGUI_LIBS=-lminigui_ths \
-		 PKG_CONFIG_PATH=$MINIGUI_LIB_DIR/pkgconfig \
-		 LIBS="-lm -lz -lpng -lts"
-
-	if [ $? != "0" ] ; then
-		echo "mgplus configure error!"
-		exit
-	fi
-
-	make 
-
-	if [ $? != "0" ] ; then
-		echo "build mgplus error!"
-		exit
-	fi
-
-	make install
-
-}
-
-function clean_mgplus()
-{
-	cd $MGPLUS_DIR
-	make distclean
-}
-
-function build_mg_samples()
-{
-	###build mg-samples
-	cd $MG_SAMPLES_DIR
-	./configure --prefix=$MINIGUI_TARGET_DIR \
-		CC=arm-linux-gnueabihf-gcc \
-		 --host=arm-linux --build=i386-linux --target=arm-linux \
-		 --with-lang=zhcn \
-		 CFLAGS=-I$MINIGUI_INC_DIR \
-		 LDFLAGS=-L$MINIGUI_LIB_DIR \
-		 MINIGUI_CFLAGS=-I$MINIGUI_INC_DIR \
-		 MINIGUI_LIBS=-lminigui_ths \
-		 PKG_CONFIG_PATH=$MINIGUI_LIB_DIR/pkgconfig \
-		 LIBS="-lm -lz -lpng -lpthread -lts"
-
-	if [ $? != "0" ] ; then
-		echo "mg-samples configure error!"
-		exit
-	fi
-
-	make 
-
-	if [ $? != "0" ] ; then
-		echo "build mg-samples error!"
-		exit
-	fi
-
-	make install
-
-}
-
-function clean_mg_samples()
-{
-	cd $MG_SAMPLES_DIR
-	make distclean
-}
-
-function build_minigui_res()
-{
-	###build minigui_res
-	cd $MINIGUI_RES_DIR
-	./configure --prefix=$MINIGUI_TARGET_DIR
-
-	if [ $? != "0" ] ; then
-		echo "minigui_ress configure error!"
-		exit
-	fi
-
-	make 
-
-	if [ $? != "0" ] ; then
-		echo "build minigui_res error!"
-		exit
-	fi
-
-	make install
-
-}
-
-function clean_minigui_res()
-{
-	cd $MINIGUI_RES_DIR
-	make distclean
-}
-
-function build_minigui()
-{
-	build_zlib
-	build_libpng
-	build_tslib
-	build_libminigui
-	build_mgplus
-	build_mg_samples
-	build_minigui_res
-}
-
-function clean_minigui()
-{
-	clean_zlib
-	clean_libpng
-	clean_tslib
-	clean_libminigui
-	clean_mgplus
-	clean_mg_samples
-	clean_minigui_res
+	cd $QT_SRC_DIR/demos/embedded/styledemo
+	export QTDIR=$QT_SRC_DIR
+	export QTEDIR=$QTDIR
+	export PATH=$QTDIR/bin:$PATH
+	export LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH 
+	qmake -project
+	qmake
+	make
 }
 
 function pack()
@@ -393,55 +232,38 @@ function pack()
 	mkdir -p $TARGET_DIR/lib/firmware
 	cp $KERNEL_DIR/out/drivers/net/wireless/esp8089/esp8089.ko $TARGET_DIR/lib/modules/
 	cp $KERNEL_DIR/drivers/net/wireless/esp8089/firmware/*.bin $TARGET_DIR/lib/firmware/
-	
-	# libz
-	cp -d $MINIGUI_TARGET_DIR/lib/libz.so $TARGET_DIR/lib/
-	cp -d $MINIGUI_TARGET_DIR/lib/libz.so.1 $TARGET_DIR/lib/
-	cp -d $MINIGUI_TARGET_DIR/lib/libz.so.1.2.11 $TARGET_DIR/lib/
 
-	# libpng12
-	cp -d $MINIGUI_TARGET_DIR/lib/libpng12.so $TARGET_DIR/lib/ 
-	cp -d $MINIGUI_TARGET_DIR/lib/libpng12.so.0 $TARGET_DIR/lib/ 
-	cp -d $MINIGUI_TARGET_DIR/lib/libpng12.so.0.37.0 $TARGET_DIR/lib/ 
+	#C++ lib
+	cp $BUILDROOT_DIR/output/host/arm-buildroot-linux-gnueabihf/sysroot/lib/libstdc++.so.6 $TARGET_DIR/lib/ -af
+	cp $BUILDROOT_DIR/output/host/arm-buildroot-linux-gnueabihf/sysroot/lib/libstdc++.so.6.0.22 $TARGET_DIR/lib/ -af
 
-	# libts
-	cp -d $MINIGUI_TARGET_DIR/lib/libts.so $TARGET_DIR/lib/ 
-	cp -d $MINIGUI_TARGET_DIR/lib/libts.so.0 $TARGET_DIR/lib/ 
-	cp -d $MINIGUI_TARGET_DIR/lib/libts.so.0.9.0 $TARGET_DIR/lib/ 
+	#qt lib
+	cp $QT_SRC_DIR/instal/lib/libQtGui.so.4 $TARGET_DIR/lib/ -af
+	cp $QT_SRC_DIR/instal/lib/libQtGui.so.4.8.6 $TARGET_DIR/lib/ -af
+	cp $QT_SRC_DIR/instal/lib/libQtNetwork.so.4 $TARGET_DIR/lib/ -af
+	cp $QT_SRC_DIR/instal/lib/libQtNetwork.so.4.8.6 $TARGET_DIR/lib/ -af
+	cp $QT_SRC_DIR/instal/lib/libQtCore.so.4 $TARGET_DIR/lib/ -af
+	cp $QT_SRC_DIR/instal/lib/libQtCore.so.4.8.6 $TARGET_DIR/lib/ -af
+	mkdir -p $TARGET_DIR/etc/fonts
+	cp $QT_SRC_DIR/demos/embedded/styledemo/styledemo $TARGET_DIR/bin/
 
-	#ts.conf
-	cp $MINIGUI_TARGET_DIR/etc/ts.conf $TARGET_DIR/etc/
-	if [ `grep -c "TSLIB_CONFFILE" $TARGET_DIR/etc/profile` -eq '0' ]; then
+	arm-linux-gnueabihf-strip $TARGET_DIR/lib/*.so
+
+	if [ `grep -c "QT_QWS_FONTDIR" $TARGET_DIR/etc/profile` -eq '0' ]; then
 		echo "" >> $TARGET_DIR/etc/profile
-		echo "export TSLIB_CONFFILE=/etc/ts.conf" >> $TARGET_DIR/etc/profile
+		echo "export QT_QWS_FONTDIR=/lib/fonts" >> $TARGET_DIR/etc/profile
+		echo "export QWS_MOUSE_PROTO=LinuxInput:/dev/input/event1" >> $TARGET_DIR/etc/profile
+
 	fi
-
-	# libminigui
-	cp -d $MINIGUI_TARGET_DIR/lib/libminigui_ths.so $TARGET_DIR/lib/
-	cp -d $MINIGUI_TARGET_DIR/lib/libminigui_ths-3.0.so.12 $TARGET_DIR/lib/
-	cp -d $MINIGUI_TARGET_DIR/lib/libminigui_ths-3.0.so.12.0.0 $TARGET_DIR/lib/
-
-	# config
-	cp $MINIGUI_TARGET_DIR/etc/MiniGUI.cfg $TARGET_DIR/etc/
 	
-	# res
-	mkdir -p $TARGET_DIR/etc/res
-	mkdir -p $TARGET_DIR/etc/res/font
-	cp -r $MINIGUI_TARGET_DIR/share/minigui/res/bmp/ $TARGET_DIR/etc/res/
-	cp -r $MINIGUI_TARGET_DIR/share/minigui/res/cursor/ $TARGET_DIR/etc/res/
-	cp -r $MINIGUI_TARGET_DIR/share/minigui/res/icon/ $TARGET_DIR/etc/res/
-	cp $MINIGUI_TARGET_DIR/share/minigui/res/font/song-12-gb2312.bin $TARGET_DIR/etc/res/font 
-	cp $MINIGUI_TARGET_DIR/share/minigui/res/font/song-16-gb2312.bin $TARGET_DIR/etc/res/font
-	
-	#mg-samples
-	cp $MG_SAMPLES_DIR/src/helloworld $TARGET_DIR/bin/
+	#remove usr/lib
+	rm -r $TARGET_DIR/usr
 }
 
 function clean_target()
 {
 	rm -r $TARGET_DIR
 }
-
 
 function flash()
 {
@@ -465,11 +287,12 @@ function flash()
 
 
 if [ $# -eq 0 ] ; then
+	build_sunxi_tools
 	build_uboot
 	build_kernel
 	build_buildroot
-	build_minigui
-	pack
+	build_qt
+	build_qt_demos
 else
 	case $1 in
 	clean)
@@ -477,8 +300,7 @@ else
 		clean_uboot
 		clean_kernel
 		clean_buildroot
-		clean_minigui
-		clean_target
+		clean_qt
 		;;
 	sunxi_tools)
 		build_sunxi_tools
@@ -492,8 +314,11 @@ else
 	buildroot)
 		build_buildroot
 		;;
-	minigui)
-		build_minigui
+	build_qt)
+		build_qt
+		;;
+	build_qt_demos)
+		build_qt_demos
 		;;
 	pack)
 		pack
